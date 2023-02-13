caf87f2c275ff2978274b4703d016ac3fe222b25
Joseph Choi <josephchoi@Josephs-MacBook-Pro-2.local>
2023-02-13 11:12:44 -0500

    second commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+    // let name = 'mario'
+
+    //making reactive by setting 
+    //setSomething is the second re-render
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    
+    const handleClick = () => {
+        setName('luigi');
+        setAge('30');
+    }
+
+    /*
+    const handleClickAgain = (name, e) => (
+        console.log('hello ' + name, e.target)
+    )
+    */
+
+    //make it reactive
+    //let name = 'mario';
+
+
     return ( 
+        
         <div className="home">
             <h2>Homepage</h2>
+            {/* using parenthesis in the event will induce it automatically, we don't want that */}
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button>
+            
+            {/* Use an anonymous function to put inputs without firing automatically */}
+            {/*<button onClick={(e) => handleClickAgain('mario', e)}>Click Me Again</button> */}
         </div>
-     );
+    );
 }
  
 export default Home;
