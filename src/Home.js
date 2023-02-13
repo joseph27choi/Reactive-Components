d538d87ccb63e2e01d04c654f2772443a4684821
Joseph Choi <josephchoi@Josephs-MacBook-Pro-2.local>
2023-02-13 17:16:58 -0500

    tutorial 18

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
+//parent component
 const Home = () => {
+    const [blogs, setBlogs] = useState(null);
 
-    // let name = 'mario'
+    useEffect(
+        () => {
+            //can do fetch data
+            fetch("http://localhost:8000/blogs")
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                });
+        },
+    /* if name changes, it will run useEffect */[]
+    );
 
-    //making reactive by setting 
-    //setSomething is the second re-render
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    
-    const handleClick = () => {
-        setName('luigi');
-        setAge('30');
-    }
-
-    /*
-    const handleClickAgain = (name, e) => (
-        console.log('hello ' + name, e.target)
-    )
-    */
-
-    //make it reactive
-    //let name = 'mario';
-
-
-    return ( 
-        
+    //anything in {} is Javascript
+    //if we remove or add any info in array, react will update
+    return (
+        //child component
         <div className="home">
-            <h2>Homepage</h2>
-            {/* using parenthesis in the event will induce it automatically, we don't want that */}
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
-            
-            {/* Use an anonymous function to put inputs without firing automatically */}
-            {/*<button onClick={(e) => handleClickAgain('mario', e)}>Click Me Again</button> */}
+            {/* using Props */}
+            {/* conditional templating */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
-}
- 
+};
+
 export default Home;
