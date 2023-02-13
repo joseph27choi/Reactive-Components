4b33fed1b4a3ac1fdb898a548ff55fb7d5cb4b89
Joseph Choi <josephchoi@Josephs-MacBook-Pro-2.local>
2023-02-13 17:35:20 -0500

    tutorial 19

--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,35 @@ import BlogList from "./BlogList";
 
 //parent component
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-    useEffect(
-        () => {
-            //can do fetch data
-            fetch("http://localhost:8000/blogs")
-                .then((res) => {
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                });
-        },
-    /* if name changes, it will run useEffect */[]
-    );
+  useEffect(() => {
+    //0.1 millisecond loading message
+    setTimeout(() => {
+      //can do fetch data
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 100);
+  }, []);
 
-    //anything in {} is Javascript
-    //if we remove or add any info in array, react will update
-    return (
-        //child component
-        <div className="home">
-            {/* using Props */}
-            {/* conditional templating */}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-        </div>
-    );
+  //anything in {} is Javascript
+  //if we remove or add any info in array, react will update
+  return (
+    //child component
+    <div className="home">
+      {/* using Props */}
+      {/* conditional templating */}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
 };
 
 export default Home;
